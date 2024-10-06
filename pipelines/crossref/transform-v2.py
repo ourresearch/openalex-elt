ebc77455120bd8a82838e3177fe715c69f70c764
Casey M <caseym@gmail.com>
2024-10-06 23:08:37 +0900

    improve parsing

--- pipelines/crossref/transform-v2.py
+++ pipelines/crossref/transform-v2.py
@@ -1,6 +1,48 @@
 import dlt
+import pyspark.sql.functions as F
 from pyspark.sql.window import Window
-from pyspark.sql.functions import row_number, col
+
+
+class CrossrefWork:
+    def __init__(self, df):
+        self.df = df
+
+    @property
+    def field_mapping(self):
+        return {
+            "doi": "DOI",
+            "title": "title",
+            "type": "type",
+            "authors": "author",
+            "abstract": "abstract"
+        }
+
+    def set_root_fields(self):
+        for field_name, field_value in self.field_mapping.items():
+            self.df = self.df.withColumn(field_name, F.col(field_value))
+
+    def set_title(self):
+        self.df = self.df.withColumn("title", F.expr("element_at(title, 1)"))
+
+    def set_authors(self):
+        self.df = self.df.withColumn(
+            "authors",
+            F.transform(
+                "author",
+                lambda x: F.create_map(
+                    F.lit("given"), x["given"],
+                    F.lit("family"), x["family"],
+                    F.lit("name"), x["name"],
+                    F.lit("ORCID"), x["ORCID"]
+                )
+            )
+        ).drop("author")
+
+    def transform(self):
+        self.set_root_fields()
+        self.set_title()
+        self.set_authors()
+        return self.df
 
 @dlt.table(
     name="crossref_works_v2",
@@ -11,19 +53,13 @@ def crossref_works_v2():
     df = dlt.read("crossref_raw_data_v2")
 
     # deduplicate by DOI, keeping the most recent indexed date DOI
-    df = df.withColumn("indexed_date", col("indexed.date-time"))
-    df = df.filter(col("indexed_date").isNotNull())
-    window_spec = Window.partitionBy("DOI").orderBy(col("indexed_date").desc())
-    df_ranked = df.withColumn("row_number", row_number().over(window_spec))
-    df_deduped = df_ranked.filter(col("row_number") == 1).drop("row_number")
+    df = df.withColumn("indexed_date", F.col("indexed.date-time"))
+    df = df.filter(F.col("indexed_date").isNotNull())
+    window_spec = Window.partitionBy("DOI").orderBy(F.col("indexed_date").desc())
+    df_ranked = df.withColumn("row_number", F.row_number().over(window_spec))
+    df_deduped = df_ranked.filter(F.col("row_number") == 1).drop("row_number")
 
     # transform the data
-    transformed_df = df_deduped.select(
-        "DOI",
-        df_deduped.title[0].alias("title"),
-        "abstract",
-        "type",
-        "author"
-    )
+    transformed_df = CrossrefWork(df_deduped).transform()
 
     return transformed_df
