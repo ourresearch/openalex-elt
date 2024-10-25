fab2d84d9c1f80812fa5bdeea3bfa678e3cd84b4
Casey M <caseym@gmail.com>
2024-10-25 16:59:04 -0500

    update ror pipeline

--- pipelines/ror.py
+++ pipelines/ror.py
@@ -1,8 +1,7 @@
 import dlt
-from pyspark.sql.functions import col
+from pyspark.sql import functions as F
 
 s3_path = "s3://openalex-ingest/ror/current/ror_snapshot.parquet"
-
 
 @dlt.table(
     name="raw_ror_data",
@@ -13,19 +12,66 @@ def load_raw_ror_data():
     df = spark.read.parquet(s3_path)
     return df
 
-
-@dlt.table(
-    name="transformed_ror_data",
-    comment="Transformed ROR data.",
-    table_properties={"quality": "silver"}
+@dlt.view(
+    name="transformed_ror_view",
+    comment="Transformed ROR data for change capture."
 )
 def transform_ror_data():
     df = dlt.read("raw_ror_data")
 
-    transformed_df = (
-        df
-        .withColumnRenamed("country_name", "country")
-        .withColumn("is_active", col("status") == "active")
+    # name
+    df = df.withColumn(
+        "name",
+        F.expr("filter(names, x -> array_contains(x.types, 'label'))[0].value")
     )
 
-    return transformed_df
+    # created and updated dates
+    df = df.withColumn("created_date", F.col("created.date")) \
+        .withColumn("updated_date", F.col("last_modified.date"))
+
+    # website
+    df = df.withColumn(
+        "website",
+        F.expr("filter(links, x -> x.type == 'website')[0].url")
+    )
+
+    # wikipedia
+    df = df.withColumn(
+        "wikipedia",
+        F.expr("filter(links, x -> x.type == 'wikipedia')[0].url")
+    )
+
+    df = df.select(
+        "id",
+        "name",
+        F.expr("locations[0].geonames_details.country_code").alias("country_code"),
+        F.expr("locations[0].geonames_details.country_name").alias("country_name"),
+        F.expr("locations[0].geonames_details.name").alias("location_name"),
+        F.expr("locations[0].geonames_details.lat").alias("latitude"),
+        F.expr("locations[0].geonames_details.lng").alias("longitude"),
+        F.expr("locations[0].geonames_id").alias("geonames_id"),
+        "types",
+        "relationships",
+        "website",
+        "wikipedia",
+        "established",
+        "external_ids",
+        "created_date",
+        "updated_date"
+    )
+
+    return df
+
+dlt.create_target_table(
+    name="transformed_ror_data",
+    comment="Up-to-date ROR data with unique IDs",
+    table_properties={"quality": "silver"}
+)
+
+dlt.apply_changes(
+    target="transformed_ror_data",
+    source="transformed_ror_view",
+    keys=["id"],
+    sequence_by="updated_date",
+    apply_as_deletes=False
+)
