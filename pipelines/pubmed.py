020ca331e59822f31f1c021db511248a218e6abb
Casey M <caseym@gmail.com>
2024-10-29 14:31:41 -0500

    format with black

--- pipelines/pubmed.py
+++ pipelines/pubmed.py
@@ -1,6 +1,60 @@
+import re
+import unicodedata
+
 import dlt
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType, BooleanType, LongType
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    ArrayType,
+    LongType,
+)
 import pyspark.sql.functions as F
+
+# normalize title UDF
+
+def clean_html(raw_html):
+    cleanr = re.compile('<\w+.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+def remove_everything_but_alphas(input_string):
+    if input_string:
+        return "".join(e for e in input_string if e.isalpha())
+    return ""
+
+def remove_accents(text):
+    normalized = unicodedata.normalize('NFD', text)
+    return ''.join(char for char in normalized if unicodedata.category(char) != 'Mn')
+
+def normalize_title(title):
+    if not title:
+        return ""
+
+    if isinstance(title, bytes):
+        title = str(title, 'ascii')
+
+    text = title[0:500]
+
+    text = text.lower()
+
+    # handle unicode characters
+    text = remove_accents(text)
+
+    # remove HTML tags
+    text = clean_html(text)
+
+    # remove articles and common prepositions
+    text = re.sub(r"\b(the|a|an|of|to|in|for|on|by|with|at|from)\b", "", text)
+
+    # remove everything except alphabetic characters
+    text = remove_everything_but_alphas(text)
+
+    return text.strip()
+
+normalize_title_udf = F.udf(normalize_title, StringType())
+
+# pubmed schema
 
 chemical_schema = StructType([
     StructField("RegistryNumber", StringType(), True),
@@ -136,7 +190,7 @@ pubmed_schema = StructType([
 @dlt.table(
     name="pubmed_landing_zone",
     comment="Landing zone for new PubMed data ingested from S3",
-    table_properties={'quality': 'bronze'}
+    table_properties={"quality": "bronze"},
 )
 def pubmed_landing_zone():
     s3_bucket_path = "s3a://openalex-ingest/pubmed"
@@ -155,13 +209,23 @@ def pubmed_landing_zone():
 
 @dlt.table(
     name="pubmed_raw_data",
-    comment="Accumulated PubMed data with unique PMID and additional metadata"
+    comment="Accumulated PubMed data with unique PMID and additional metadata",
 )
 def pubmed_raw_data():
     df = dlt.read_stream("pubmed_landing_zone")
 
     # deduplicate by PMID and revised date
-    df = df.withColumn("revised_date", F.to_date(F.concat_ws("-", df.MedlineCitation.DateRevised.Year, df.MedlineCitation.DateRevised.Month, df.MedlineCitation.DateRevised.Day)))
+    df = df.withColumn(
+        "revised_date",
+        F.to_date(
+            F.concat_ws(
+                "-",
+                df.MedlineCitation.DateRevised.Year,
+                df.MedlineCitation.DateRevised.Month,
+                df.MedlineCitation.DateRevised.Day,
+            )
+        ),
+    )
     df = df.withColumn("pmid", df.MedlineCitation.PMID._VALUE)
     df = df.dropDuplicates(["pmid", "revised_date"])
 
@@ -173,93 +237,101 @@ def pubmed_raw_data():
     comment="Transformed view of the raw PubMed data",
 )
 def pubmed_transformed_view():
-    # Helper functions for cleaner code
     def create_date_column(year_col, month_col, day_col):
         return F.to_date(
-            F.concat_ws("-",
-                        F.col(year_col),
-                        F.col(month_col),
-                        F.col(day_col)
-                        )
+            F.concat_ws("-", F.col(year_col), F.col(month_col), F.col(day_col))
         )
 
     def extract_id_by_type(id_type):
-        return F.expr(f"filter(PubmedData.ArticleIdList.ArticleId, x -> x._IdType = '{id_type}')[0]._VALUE")
+        return F.expr(
+            f"filter(PubmedData.ArticleIdList.ArticleId, x -> x._IdType = '{id_type}')[0]._VALUE"
+        )
 
     # Start transformation
-    df = (dlt.read_stream("pubmed_raw_data")
-          # basic article metadata
-          .withColumn("title", F.col("MedlineCitation.Article.ArticleTitle"))
-          .withColumn("abstract", F.col("MedlineCitation.Article.Abstract.AbstractText"))
-          .withColumn("type", F.col("MedlineCitation.Article.PublicationTypeList.PublicationType._VALUE"))
-
-          # author information
-          .withColumn("authors", F.col("MedlineCitation.Article.AuthorList"))
-
-          # journal information
-          .withColumn("source_title", F.col("MedlineCitation.Article.Journal.Title"))
-          .withColumn("source_issns", F.col("MedlineCitation.Article.Journal.ISSN._VALUE"))
-          .withColumn("volume", F.col("MedlineCitation.Article.Journal.JournalIssue.Volume"))
-          .withColumn("issue", F.col("MedlineCitation.Article.Journal.JournalIssue.Issue"))
-
-          # ids
-          .withColumn("doi", extract_id_by_type("doi"))
-          .withColumn("pmc_id", extract_id_by_type("pmc"))
-
-          .withColumn("references", F.col("PubmedData.ReferenceList"))
-
-          # dates
-          .withColumn("publication_date",
-                      create_date_column(
-                          "MedlineCitation.Article.Journal.JournalIssue.PubDate.Year",
-                          "MedlineCitation.Article.Journal.JournalIssue.PubDate.Month",
-                          "MedlineCitation.Article.Journal.JournalIssue.PubDate.Day"
-                      )
-                      )
-          .withColumn("created_date",
-                      create_date_column(
-                          "MedlineCitation.DateCompleted.Year",
-                          "MedlineCitation.DateCompleted.Month",
-                          "MedlineCitation.DateCompleted.Day"
-                      )
-                      )
-          .withColumn("updated_date",
-                      create_date_column(
-                          "MedlineCitation.DateRevised.Year",
-                          "MedlineCitation.DateRevised.Month",
-                          "MedlineCitation.DateRevised.Day"
-                      )
-                      )
-          )
+    df = (
+        dlt.read_stream("pubmed_raw_data")
+        # basic article metadata
+        .withColumn("title", F.col("MedlineCitation.Article.ArticleTitle"))
+        .withColumn(
+            "normalized_tital",
+            normalize_title_udf(F.col("MedlineCitation.Article.ArticleTitle")),
+        )
+        .withColumn("abstract", F.col("MedlineCitation.Article.Abstract.AbstractText"))
+        .withColumn(
+            "type",
+            F.col("MedlineCitation.Article.PublicationTypeList.PublicationType._VALUE"),
+        )
+        # author information
+        .withColumn("authors", F.col("MedlineCitation.Article.AuthorList"))
+        # journal information
+        .withColumn("source_title", F.col("MedlineCitation.Article.Journal.Title"))
+        .withColumn(
+            "source_issns", F.col("MedlineCitation.Article.Journal.ISSN._VALUE")
+        )
+        .withColumn(
+            "volume", F.col("MedlineCitation.Article.Journal.JournalIssue.Volume")
+        )
+        .withColumn(
+            "issue", F.col("MedlineCitation.Article.Journal.JournalIssue.Issue")
+        )
+        # ids
+        .withColumn("doi", extract_id_by_type("doi"))
+        .withColumn("pmc_id", extract_id_by_type("pmc"))
+        .withColumn("references", F.col("PubmedData.ReferenceList"))
+        # dates
+        .withColumn(
+            "publication_date",
+            create_date_column(
+                "MedlineCitation.Article.Journal.JournalIssue.PubDate.Year",
+                "MedlineCitation.Article.Journal.JournalIssue.PubDate.Month",
+                "MedlineCitation.Article.Journal.JournalIssue.PubDate.Day",
+            ),
+        )
+        .withColumn(
+            "created_date",
+            create_date_column(
+                "MedlineCitation.DateCompleted.Year",
+                "MedlineCitation.DateCompleted.Month",
+                "MedlineCitation.DateCompleted.Day",
+            ),
+        )
+        .withColumn(
+            "updated_date",
+            create_date_column(
+                "MedlineCitation.DateRevised.Year",
+                "MedlineCitation.DateRevised.Month",
+                "MedlineCitation.DateRevised.Day",
+            ),
+        )
+    )
 
     return df.select(
-        # ids
-        "pmid", "doi", "pmc_id",
-
-        # core article metadata
-        "title", "abstract", "type",
-
-        # author information
+        "pmid",
+        "doi",
+        "pmc_id",
+        "title",
+        "abstract",
+        "type",
         "authors",
-
-        # journal metadata
-        "source_title", "source_issns",
-        "volume", "issue",
-
-        # dates
-        "publication_date", "created_date", "updated_date"
+        "source_title",
+        "source_issns",
+        "volume",
+        "issue",
+        "publication_date",
+        "created_date",
+        "updated_date",
     )
 
 
 dlt.create_target_table(
     name="pubmed_works",
     comment="Final PubMed articles table with unique PMIDs",
-    table_properties={"quality": "silver"}
+    table_properties={"quality": "silver"},
 )
 
 dlt.apply_changes(
     target="pubmed_works",
     source="pubmed_transformed_view",
     keys=["pmid"],
-    sequence_by="updated_date"
+    sequence_by="updated_date",
 )
